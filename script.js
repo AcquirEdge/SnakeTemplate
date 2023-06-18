3b01700801b8cae6e370ea715ed833f9383a7d84
Allan <allanwu12321@gmail.com>
2023-06-18 15:33:08 -0700

    Completed updateBoard() and generateRandomApple() and main() functionality

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let direction;
 
 // We need these global variables to help us keep track of where the snake and apple are at all times
 let snake = [];
+// apple is going to be an object representing a coordinate, with an 'x' property and 'y' property.
 let apple;
 
 /**
@@ -38,13 +39,32 @@ function initializeBoard() {
     let snakeSegment1 = { x: centerX + 1, y: centerY };
     let snakeSegment2 = { x: centerX + 2, y: centerY };
     snake = [ head, snakeSegment1, snakeSegment2 ]
+}
 
-    // The snake is stored separately from the board, so we must update the board whenever the state of the snake changes
-    for(let segment of snake){
-        board[segment.y][segment.x] = SNAKE_BODY;
+/**
+ * Helper function to update our board when the state of the snake or apple changes.
+ * 
+ * What this function is going to accomplish is a few things:
+ * 1. First, we we have to wipe the board clean, or change the tiles currently occupied by the snake and apple clean
+ *      - This is because if we do not wipe the board first, there will be lingering snake segments or old apples remaining on the board
+ * 2. Then, we iterate through the snake, and update the board with the new position of our snake.
+ * 3. Finally, we update the board with the new position of our apple
+ * 
+ * Our board should now have updated.
+ */
+function updateBoard(){
+    // Clear our board
+    for(let i = 0; i < BOARD_SIZE; i++){
+        for(let j = 0; j < BOARD_SIZE; j++){
+            board[i][j] = 0;
+        }
     }
-
-    // Generate a random apple on the board
+    // Update the board with the new position of our snake
+    snake.forEach((segment) => {
+        board[segment.y][segment.x] = SNAKE_BODY;
+    })
+    // Update the board with the new position of our apple
+    board[apple.y][apple.x] = APPLE_CELL;
 }
 
 /**
@@ -75,5 +95,54 @@ function printBoard() {
     console.log("--".repeat(BOARD_SIZE + 1));
 }
 
-initializeBoard();
-printBoard();
+/**
+ * Function that generates a new random apple on our board.
+ * 
+ * This function is going to work very similarly to the generateRandomTile function from 2048.
+ * There are some differences, however. Remember that there can only be one apple on the board at one time.
+ * This means that in order to generate a new apple, you must either make sure that the apple is removed beforehand.
+ * 
+ * Instead of directly updating the board, we're going to utilize our updateBoard function to do so when we generate a new Apple
+ * 
+ */
+function generateRandomApple(){
+    // Remove the apple if one exists. This prevents us from having multiple apples on our board
+    if(apple){
+        board[apple.y][apple.x] = EMPTY_CELL;
+        apple = null;
+    }
+    // Finding all the empty cells in the board
+    let emptyCells = [];
+    for(let y = 0; y < BOARD_SIZE; y++){
+        for(let x = 0; x < BOARD_SIZE; x++){
+            if(board[y][x] === 0){
+                let coordinate = {
+                    x: x,
+                    y: y
+                }
+                emptyCells.push(coordinate);
+            }
+        }
+    }
+    // Choose a random cell 
+    if(emptyCells.length > 0){
+        let randomCellIndex = Math.floor(Math.random() * emptyCells.length);
+        let randomCellCoordiante = emptyCells[randomCellIndex];
+        apple = randomCellCoordiante;
+    }
+    // Since we made a change to the apple, we update the board;
+    updateBoard();
+}
+
+/**
+ * Main function to run our game.
+ * Place all the functions necessary to run the game (so far) into this function
+ */
+function main(){
+    initializeBoard();
+    generateRandomApple();
+    updateBoard();
+    printBoard();
+}
+
+main();
