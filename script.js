c8c66e294329b502c25541b6648ee777ad4199a4
Allan <allanwu12321@gmail.com>
2023-06-21 19:08:00 -0700

    Please see comments for: printBoard(), moveSnake(), getMove() for the next phase of the game

--- script.js
+++ script.js
@@ -65,11 +65,11 @@ function updateBoard(){
             board[i][j] = 0;
         }
     }
-    // Update the board with the new position of our snake
+    // Update the board with the position of our snake
     snake.forEach((segment) => {
         board[segment.y][segment.x] = SNAKE_BODY;
     })
-    // Update the board with the new position of our apple
+    // Update the board with the position of our apple
     board[apple.y][apple.x] = APPLE_CELL;
 }
 
@@ -85,6 +85,8 @@ function updateBoard(){
  * Finally, each cell on the board is going to be separated by a " " in between. This is again to improve visibility.
  */
 function printBoard() {
+    // Uncomment the next line to run a smoother game:
+    // console.clear();
     // This will provide the top dashed line to our board
     console.log("--".repeat(BOARD_SIZE + 1))
     for(let i = 0; i < BOARD_SIZE; i++){
@@ -147,17 +149,29 @@ function generateRandomApple(){
  * 
  * I've provided some guiding comments to help direct you in the correct direction
  * 
+ * Adjust getMove to now accommodate for our new moveSnake() function:
+ * 
+ * After moving the snake:
+ * 
+ * If the snake landed on an apple, update the board and generate a new apple
+ * If the snake was bitten or runs into a wall, Print out "Loss", close the readline interface, and return
+ * Otherwise, don't do anything.
+ * 
  */
 function getMove() {
     // use readline's interface to ask retrieve the user's next move
     rl.question("Input your next move (up, down, left, right, or nothing to move the snake forward): ", (answer) => {
         // Depending on the user's answer, we change the snake's direction
         changeDirection(answer);
-        // After changing the snake's direction leave a console.log to simulate moving the snake forward
-        // We will take care of eating apples later on
+        
+        // Move the snake forward and save the result
 
-        console.log("moving snake forward");
-        // After all of this happens, make sure we print out our board, and recursively call this function again.
+        // If the snake lands on an apple, update board and generate a new apple
+        // We need to update the board first since generateRandomApple uses the state of the board
+
+        // Else, if the snake bites itself or runs into a wall, print out "Loss"
+
+        // Otherwise, we just continue as normal.
         updateBoard();
         printBoard();
         getMove();
@@ -183,21 +197,92 @@ function changeDirection(newDir){
             }
             break;
         case "down":
-            if(direction !== "down"){
-                direction = "up";
+            if(direction !== "up"){
+                direction = "down";
             }
             break;
         case "left":
-            if(direction !== "down"){
-                direction = "up";
+            if(direction !== "right"){
+                direction = "left";
             }
             break;
         case "right": 
-            if(direction !== "down"){
-                direction = "up";
+            if(direction !== "left"){
+                direction = "right";
             }
             break;
     }
+}
+
+/**
+ * Function to move the snake forward one game tick. In the console version, the game is going to move forward one single tick at a time.
+ * Once we move the game to the Browser, the snake will move forwards on its own, but for now, the snake will move one step at a time.
+ * 
+ * I will provide you with some pseudocode to help you complete this function, and some reasoning about why we do it this way:
+ * 
+ * We are going to be working with our global snake variable. Every time we make a move, we will update the board with the new state.
+ * The reason for this is that there is no good way to store the snake directly in the board and refer back to it later.
+ * 
+ * First, we have to fully move the snake before we check anything.
+ * If we land on an apple, we can't add a segment to the end of the snake until all the other segments complete moving.
+ * Furthermore, there is an edge case:
+ *      What if the block in front of the snake is its very last body segment? 
+ *      If we do not complete the full move of the snake before checking to see if the snake bit itself, then the snake will be 
+ *      biting a portion of itself that wouldn't be there if the snake finishes moving. 
+ * 
+ * Next, how would we accomplish moving the snake? 
+ *      Essentially, we move the snake's head in the current direction, then we simply tell the rest of the snake to follow the 
+ *      segment directly in front of it. This is one of the reasons why we keep the snake as an array, so it maintains its order.
+ *      If we stored the snake data directly in the board, similarly to 2048, we would have little way to keep track of its order.
+ * 
+ * Finally, when we finish moving the snake, one of 4 things can possibly happen: 
+ * 1. The snake head lands on an apple. 
+ * 2. The snake head lands on a segment of itself
+ * 3. The snake head hits a wall.
+ * 4. None of the above happens, and the game continues as normal. 
+ * 
+ * Let's think about what we do in each of these cases:
+ * Case #1: 
+ *      Append the copy of the last segment to the end of the snake. Set the return value to "Apple"
+ * 
+ * Case #2 and #3:
+ *      Set the return value to "Bite" or "Wall", respectively.
+ * 
+ * Case #4:
+ *      Nothing. We can either set the return value to signify this, or do nothing. 
+ *      In our case, we will opt to set the return value to "Continue". 
+ * 
+ * So our function flow is going to look like this:
+ * Move the head of the snake > Make all the other segments follow > We check the state of the game, and return the corresponding value.
+ * 
+ * @returns {string} - The result of the move:
+ *                  "apple": Snake ate an apple, 
+ *                  "bite": Snake bites itself, 
+ *                  "wall": Snake hits a wall, 
+ *                  "continue": Nothing happens
+ */
+
+function moveSnake() {
+    // Declare our return variable "result" and give it a default value of "continue".
+    // Declare a temporary variable "lastMoved" that holds the last segment that has moved. The initial value will be the head of the snake.
+    //      Reason: Once a segment moves, we need to keep track of where it was, or else the next segment doesn't know where to go to.
+    //              Furthermore, after the snake finishes moving, this will hold the segment to be appended if the snake eats an apple.
+    // Depending on the current direction, adjust the snake head segment's x and y value. You may need to use a switch statement here
+    // For each segment:
+        // If it is the first segment (head of the snake):
+            // use a switch statement to adjust the head segment's x and y value.
+        // Otherwise
+            // Declare a temporary variable "tempCopy" to hold a copy of the current segment.
+            // Set the current segment equal to "lastMoved". This essentially moves the segment forward.
+            // Set "lastMoved" equal to "tempCopy". Thus, "lastMoved" now holds a temporary copy of the segment that just moved.
+    // If the snake's head has the same coordinates as the apple (We can't use the board to check it since the board can only hold one value at a time)
+        // Set "result" to "apple"
+        // Push "lastMoved" to the end of the snake array
+    // Else, if the snake's head has coordinates that exceed our board
+        // Set the "result" to "wall"
+    // Else, if the snake's head has coordinates that match one of its body parts
+        // Set the "result" to "bite"
+    // Return "result"
 }
 
 /**
